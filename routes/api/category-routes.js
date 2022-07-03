fcf5df86ef7d5dfcf093c854d0858c1b14c00ac8
Anthony Poku <tonypoku@gmail.com>
2022-07-03 08:17:06 -0500

    category-routes completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,8 +48,25 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const category = await Category.findByPk(req.params.id);
+
+    if (!category) {
+      res
+        .status(404)
+        .json({ message: `No category found with id ${eq.params.id}` });
+
+      return;
+    }
+
+    await category.destroy();
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
